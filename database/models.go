3ed2a66acdd3379d2107a7baaee6f20413b22439
Jan <janbar2001@gmail.com>
2024-12-14 17:21:31 +0100

    some further moddeling

--- database/models.go
+++ database/models.go
@@ -1,20 +1,25 @@
 package database
 
+import "time"
+
 type Account struct {
-	Number int
-	Name   string
-	Kind   string
+	ID   uint
+	Name string
+	Kind string
 }
 
 type Transaction struct {
+	ID            uint
 	Amount        float32
 	Debit         bool
-	OffsetAccount int
-	Account       int
+	OffsetAccount uint
+	Account       uint
+	Time          time.Time
 	Description   string
 }
 
 type User struct {
+	ID       uint
 	Name     string
 	Password string
 }
